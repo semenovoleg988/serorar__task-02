388beb234d7c637fe1adaa4d90a3eafa1f99119d
semenovoleg98 <semenovoleg988@gmail.com>
2020-06-23 23:12:02 +0300

    initial commit

---
+++ ClientApp/ServiceProcessingWithoutJquery.js
@@ -0,0 +1,82 @@
+﻿var params = { 'mark': 5 };
+
+function grater_than_clear() {
+
+    inputData = document.getElementById("grater_input_clear").value;
+    params = { 'mark': inputData };
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'http://localhost:63414/StudentListWebService.asmx/GetStudentsGraterThan');
+
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+    xhr.send(JSON.stringify(params));
+
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            var result = JSON.parse(xhr.response).d
+            var trHTML = '<tbody><tr><th>Name</th><th>AvgMark</th></tr>';
+            result.forEach(element => trHTML += '<tr><td>' + element.Name + '</td><td>' + element.AvgMark + '</td></tr>')
+            trHTML += "</tbody >";
+            document.getElementById('grater_records_table_clear').innerHTML = trHTML;
+        }
+    };
+
+}
+
+function lower_than_clear() {
+
+    inputData = document.getElementById("lower_input_clear").value;
+    params = { 'mark': inputData };
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'http://localhost:63414/StudentListWebService.asmx/GetStudentsLowerThan');
+
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+    xhr.send(JSON.stringify(params));
+
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            console.log(JSON.parse(xhr.response));
+            var result = JSON.parse(xhr.response).d
+            var trHTML = '<tbody><tr><th>Name</th><th>AvgMark</th></tr>';
+            result.forEach(element => trHTML += '<tr><td>' + element.Name + '</td><td>' + element.AvgMark + '</td></tr>')
+            trHTML += "</tbody >";
+            document.getElementById('lower_records_table_clear').innerHTML = trHTML;
+        }
+    };
+}
+
+function in_range_clear() {
+    inputDataMin = document.getElementById("range_input_min_clear").value;
+    inputDataMax = document.getElementById("range_input_max_clear").value;
+    params = { 'minMark': inputDataMin, 'maxMark': inputDataMax };
+    
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'http://localhost:63414/StudentListWebService.asmx/GetStudentsInRange');
+
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+    xhr.send(JSON.stringify(params));
+
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            console.log(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            var result = JSON.parse(xhr.response).d
+            var trHTML = '<tbody><tr><th>Name</th><th>AvgMark</th></tr>';
+            result.forEach(element => trHTML += '<tr><td>' + element.Name + '</td><td>' + element.AvgMark + '</td></tr>')
+            trHTML += "</tbody >";
+            document.getElementById('range_records_table_clear').innerHTML = trHTML;
+        }
+    };
+
+}
